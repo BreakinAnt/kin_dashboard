fbd710ca9cec61b634d02af269e491d67ce26351
Victor Ribeiro <devjvribeiro@gmail.com>
2022-11-06 15:10:22 -0300

    style: formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
-import { InertiaProgress } from '@inertiajs/progress'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
 createInertiaApp({
   resolve: name => import(`../views/adminkit/${name}`),
@@ -8,9 +8,9 @@ createInertiaApp({
     const VueApp = createApp({ render: () => h(App, props) });
 
     VueApp.use(plugin)
-        .mixin({ methods: { route: window.route } })
-        .mount(el);
+      .mixin({ methods: { route: window.route } })
+      .mount(el);
   },
-})
+});
 
-InertiaProgress.init()
+InertiaProgress.init();
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,29 +1,21 @@
-const path = require('path')
+const path = require('path');
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/adminkit/js/app.js', 'public/adminkit/js')
+
     .alias({
         '@adminkit': path.resolve(__dirname, 'resources/adminkit'),
         '@adminkit-components': path.resolve(__dirname, 'resources/components/adminkit')
     })
     .vue()
+
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/adminkit/scss/app.scss', 'public/adminkit/css')
-    .disableSuccessNotifications()
     .copy('resources/adminkit/img', 'public/adminkit/img')
     .copy('resources/adminkit/fonts', 'public/adminkit/fonts')
     .browserSync({
-        proxy: "127.0.0.1:8000"
-    });
+        proxy: '127.0.0.1:8000',
+    })
+
+    .disableSuccessNotifications();
